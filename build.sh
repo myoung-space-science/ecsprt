9ef8a540ef8e5cb0b04534e3177ade834976ee5a
Matthew Young <myoung.space.science@gmail.com>
2023-12-08 10:59:44 -0500

    Add build.sh and run.sh

---
+++ build.sh
@@ -0,0 +1,278 @@
+#!/bin/bash
+
+# See https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html
+# - e => Exit immediately if a pipeline returns non-zero status.
+# - u => Treat unset variables and parameters (with certain exceptions) as
+#        errors when performing parameter expansion.
+set -eu
+
+# Define special flag to indicate success.
+success="@!SUCCESS!@"
+
+# Define special unicode characters.
+u_warning="\U1F937"
+u_success="\u2728"
+u_error="\U1F62d"
+
+# Declare the name of the target program.
+prog=
+cli=${0##*/}
+
+# Declare build-related directories and files.
+cwd=$(pwd)
+srcdir=${cwd}/src
+bindir=${cwd}/bin
+mkdir -p "${bindir}"
+buildlog="build.log"
+> ${buildlog}
+
+# Set option defaults.
+verbose=0
+debug=0
+optimize=0
+log_info=1
+log_none=0
+log_enter_exit=0
+from_clean=0
+
+# Define text formatting commands.
+# - textbf: Use bold-face.
+# - textnm: Reset to normal.
+# - startul: Start underlined text.
+# - endul: End underlined text.
+textbf=$(tput bold)
+textnm=$(tput sgr0)
+startul=$(tput smul)
+endul=$(tput rmul)
+
+# This is the CLI's main help text.
+show_help()
+{
+    echo "
+${textbf}NAME${textnm}
+        $cli - Build the hybrid simulation or potential solver
+
+${textbf}SYNOPSIS${textnm}
+        ${textbf}$cli${textnm} -p {pic,solver} [${startul}OPTIONS${endul}]
+
+${textbf}DESCRIPTION${textnm}
+        This script will perform all the steps to build TARGET.
+
+        ${textbf}-h${textnm}, ${textbf}--help${textnm}
+                Display help and exit.
+        ${textbf}-p${textnm}, ${textbf}--program${textnm}=${startul}PROG${endul}
+                The target program (required).
+        ${textbf}--log${textnm}=${startul}LEVEL${endul}
+                Print informational messages from one of the following categories. May be
+                specified multiple times, with precedence from last to first. The default
+                behavior is equivalent to --log 'info'.
+                'info': print general runtime information.
+                'none': suppress all custom messages.
+                'enter-exit': print a notification upon entry and exit of labeled functions.
+        ${textbf}--from-clean${textnm}
+                Run make clean in the target src directory before building
+                the executable.
+        ${textbf}--debug${textnm}
+                Compile with debugging flags.
+        ${textbf}--optimize${textnm}
+                Compile with optimization flags.
+        ${textbf}-v${textnm}, ${textbf}--verbose${textnm}
+                Print informative messages during the build process.
+"
+}
+
+# This will run if the CLI gets an unrecognized option.
+report_bad_arg()
+{
+    printf "\nUnrecognized command: ${1}\n\n"
+}
+
+# Read command-line arguments.
+TEMP=$(getopt \
+    -n 'build.sh' \
+    -o 'hvp:' \
+    -l 'help,verbose' \
+    -l 'program:' \
+    -l 'log:' \
+    -l 'from-clean' \
+    -l 'debug,optimize' \
+    -- "$@")
+
+if [ $? -ne 0 ]; then
+    echo "ERROR: Failed to parse command-line arguments."
+    exit 1
+fi
+
+eval set -- "$TEMP"
+unset TEMP
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        '-h'|'--help')
+            show_help
+            exit
+        ;;
+        '-p'|'--program')
+            prog="${2}"
+            shift 2
+            continue
+        ;;
+        '--log')
+            case "$2" in
+                'info')
+                    log_info=1
+                    log_none=0
+                ;;
+                'enter-exit')
+                    log_enter_exit=1
+                    log_none=0
+                ;;
+                'none')
+                    log_info=0
+                    log_enter_exit=0
+                ;;
+            esac
+            shift 2
+            continue
+        ;;
+        '--from-clean')
+            from_clean=1
+            shift
+            continue
+        ;;
+        '--debug')
+            debug=1
+            shift
+            continue
+        ;;
+        '--optimize')
+            optimize=1
+            shift
+            continue
+        ;;
+        '-v'|'--verbose')
+            verbose=1
+            shift
+            continue
+        ;;
+        '--')
+            shift
+            break
+        ;;
+    esac
+done
+
+# Alert the user to unrecognized arguments.
+extra_args="$@"
+if [ -n "${extra_args}" ]; then
+    show_help
+    echo
+    echo -e "${u_warning} Got the following unrecognized argument(s):"
+    echo
+    for arg in ${extra_args[@]}; do
+        echo ">   $arg"
+    done
+    echo
+    echo "Possible causes of this error:"
+    echo "- This script does not accept any positional arguments. "
+    echo "  Please pass the required program name via the -p/--program option."
+    echo "- This script does not support passing arbitrary arguments to ${textbf}make${textnm}."
+    echo "  If you want (and know how to) build the code in a way that this script does not provide, "
+    echo "  you must directly call ${textbf}make${textnm} with appropriate arguments."
+    echo
+    exit 1
+fi
+
+stage=
+
+cleanup() {
+    if [ "${stage}" != "${success}" ]; then
+        if [ -n "${stage}" ]; then
+            exitmsg="${stage} stage failed. See ${buildlog} for details."
+        else
+            exitmsg="Unknown error."
+        fi
+        echo 
+        echo -e ${u_error} $exitmsg
+        exit 1
+    else
+        echo 
+        echo -e "${u_success} Success! ${u_success}"
+        echo 
+        if [ $debug == 1 ]; then
+            echo "For debugging on a single processor, try"
+            echo "$ gdb --args ${bindir}/${prog} [ARGS]"
+        else
+            echo "To run on a single processor, try"
+            echo "$ ${bindir}/${prog} [ARGS]"
+            echo 
+            echo "To run on N processors, try"
+            echo "$ mpiexec -n N ${bindir}/${prog} [ARGS]"
+        fi
+    fi
+}
+
+trap cleanup EXIT
+
+mark_stage() {
+    stage="${1}"
+    if [ $verbose == 1 ]; then
+        echo "[${cli}] Executing ${stage} stage"
+    fi
+}
+
+stage="setup"
+
+# Refuse to run without a target program.
+if [ -z "${prog}" ]; then
+    show_help
+    echo
+    echo "ERROR: Missing target program." &> ${buildlog}
+    exit 1
+fi
+
+if [ ${from_clean} == 1 ]; then
+    # Mark this stage.
+    mark_stage "clean"
+    pushd "${srcdir}" &> /dev/null \
+    && make clean &> ${cwd}/${buildlog} \
+    && popd &> /dev/null
+fi
+
+# Mark this stage.
+mark_stage "build"
+
+# Build the executable in the source directory.
+cd ${srcdir}
+cppflags=
+cflags=
+progext=
+if [ ${debug} == 1 ]; then
+    cppflags="${cppflags} -DDEBUG"
+    cflags="${cflags} -g -O0"
+    log_enter_exit=1
+    progext="-dbg"
+fi
+if [ ${optimize} == 1 ]; then
+    cppflags="${cppflags} -DNDEBUG"
+    cflags="${cflags} -O3"
+    progext="-opt"
+fi
+if [ ${log_none} == 1 ]; then
+    cppflags="${cppflags} -DSILENT"
+else
+    if [ ${log_enter_exit} == 1 ]; then
+        cppflags="${cppflags} -DLOG_ENTER_EXIT"
+    fi
+fi
+make ${prog} CPPFLAGS="${cppflags}" CFLAGS="${cflags}" &> ${cwd}/${buildlog}
+
+if [ -n "${progext}" ]; then
+    pushd ${bindir} &> /dev/null \
+    && /bin/cp ${prog} ${prog}${progext} \
+    && popd &> /dev/null
+fi
+
+# Signal success to the clean-up function.
+stage=$success
+
---
+++ run.sh
@@ -0,0 +1,253 @@
+#!/bin/bash
+
+# See https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html
+# - e => Exit immediately if a pipeline returns non-zero status.
+# - u => Treat unset variables and parameters (with certain exceptions) as
+#        errors when performing parameter expansion.
+set -eu
+
+# Define special flag to indicate success.
+success="@!SUCCESS!@"
+
+# Define special unicode characters.
+u_warning="\U1F937"
+u_success="\u2728"
+u_error="\U1F62d"
+
+# Declare the name of the target program.
+prog=
+cli=${0##*/}
+
+# Declare run-related directories and files.
+cwd=$(pwd)
+srcdir=${cwd}/src
+bindir=${cwd}/bin
+logname="run.log"
+runlog=
+
+# Set option defaults.
+np=1
+verbose=0
+debug=0
+rundir='.'
+outdir=
+options=
+args=
+
+# Define text formatting commands.
+# - textbf: Use bold-face.
+# - textnm: Reset to normal.
+# - startul: Start underlined text.
+# - endul: End underlined text.
+textbf=$(tput bold)
+textnm=$(tput sgr0)
+startul=$(tput smul)
+endul=$(tput rmul)
+
+# This is the CLI's main help text.
+show_help()
+{
+    echo "
+${textbf}NAME${textnm}
+        $cli - Run the hybrid simulation or potential solver
+
+${textbf}SYNOPSIS${textnm}
+        ${textbf}$cli${textnm} -p {pic,solver} [${startul}OPTIONS${endul}] [args ${startul}ARGS${endul}]
+
+${textbf}DESCRIPTION${textnm}
+        This script will run the target program. All arguments after 'args' will pass through.
+
+        ${textbf}-h${textnm}, ${textbf}--help${textnm}
+                Display help and exit.
+        ${textbf}-p${textnm}, ${textbf}--program${textnm} ${startul}PROG${endul}
+                The target program (required).
+        ${textbf}--debug${textnm}
+                Run ${startul}PROG${endul} in debugging mode.
+        ${textbf}-n${textnm}, ${textbf}--nproc${textnm} ${startul}N${endul}
+                Number of processors to use.
+                (default: ${np})
+        ${textbf}-r${textnm}, ${textbf}--rundir${textnm} ${startul}RUNDIR${endul}
+                Name of the parent directory that should contain the 
+                run-specific directory. See ${startul}OUTDIR${endul}.
+                (default: ${rundir})
+        ${textbf}-o${textnm}, ${textbf}--outdir${textnm} ${startul}OUTDIR${endul}
+                Name of the run-specific directory, within ${startul}RUNDIR${endul}, that will
+                contain the output of this run.
+                The default name is generated from the current date and time.
+        ${textbf}--options${textnm} ${startul}OPTIONS${endul}
+                Full name of the file that contains PETSc options.
+                (default: none)
+        ${textbf}-v${textnm}, ${textbf}--verbose${textnm}
+                Print informative messages during the execution process.
+"
+}
+
+# This will run if the CLI gets an unrecognized option.
+report_bad_arg()
+{
+    printf "\nUnrecognized command: ${1}\n\n"
+}
+
+# Read command-line arguments.
+TEMP=$(getopt \
+    -n 'run.sh' \
+    -o '+hvp:n:r:o:' \
+    -l 'help,verbose' \
+    -l 'program:' \
+    -l 'debug' \
+    -l 'nprocs:' \
+    -l 'rundir:' \
+    -l 'outdir:' \
+    -l 'options:' \
+    -- "$@")
+
+if [ $? -ne 0 ]; then
+    echo "ERROR: Failed to parse command-line arguments."
+    exit 1
+fi
+
+eval set -- "$TEMP"
+unset TEMP
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        '-h'|'--help')
+            show_help
+            exit
+        ;;
+        '-p'|'--program')
+            prog="${2}"
+            shift 2
+            continue
+        ;;
+        '--debug')
+            debug=1
+            shift
+            continue
+        ;;
+        '-n'|'--nproc')
+            np="${2}"
+            shift 2
+            continue
+        ;;
+        '-r'|'--rundir')
+            rundir="${2}"
+            shift 2
+            continue
+        ;;
+        '-o'|'--outdir')
+            outdir="${2}"
+            shift 2
+            continue
+        ;;
+        '--options')
+            options=$(readlink -f "${2}")
+            shift 2
+            continue
+        ;;
+        '-v'|'--verbose')
+            verbose=1
+            shift
+            continue
+        ;;
+        '--')
+            shift
+            break
+        ;;
+    esac
+done
+
+# Convert extra arguments to program arguments.
+extra="$@"
+args=${extra//args}
+
+stage=
+
+cleanup() {
+    if [ "${stage}" != "${success}" ]; then
+        if [ -n "${stage}" ]; then
+            exitmsg="${stage} stage failed. See ${runlog} for details."
+        else
+            exitmsg="Unknown error."
+        fi
+        echo 
+        echo -e ${u_error} $exitmsg
+        exit 1
+    else
+        echo 
+        echo -e "${u_success} Success! ${u_success}"
+        echo 
+        echo "Output is in ${dstdir}"
+    fi
+}
+
+trap cleanup EXIT
+
+mark_stage() {
+    stage="${1}"
+    if [ $verbose == 1 ]; then
+        echo "[${cli}] Executing ${stage} stage"
+    fi
+}
+
+# Define a temporary run log path, in case setup fails.
+runlog=${rundir}/${logname}
+
+# Mark this stage.
+mark_stage "setup"
+
+# Set the local name of the output directory.
+if [ -z "${outdir}" ]; then
+    dstdir=${rundir}/$(date +'%Y-%m-%d-%H%M%S')
+else
+    dstdir=${rundir}/"${outdir}"
+fi
+
+# Create the full output directory.
+mkdir -p ${dstdir}
+
+# Redefine the run log path.
+runlog=${dstdir}/${logname}
+
+# Refuse to run without a target program.
+if [ -z "${prog}" ]; then
+    show_help
+    echo
+    echo "ERROR: Missing target program." &> ${runlog}
+    exit 1
+fi
+
+# Mark this stage.
+mark_stage "symlink"
+
+# Create a symlink to this run in the directory of runs.
+pushd ${rundir} &> /dev/null
+rm -f latest
+ln -s ${dstdir} latest
+popd &> /dev/null
+
+# Move to the output directory.
+pushd ${dstdir} &> /dev/null
+
+# Mark this stage.
+mark_stage "run"
+
+# Run the program.
+if [ -n "${options}" ]; then
+    /bin/cp "${options}" petsc.ini &> run.log
+fi
+if [ ${debug} == 1 ]; then
+    if [ ${np} -gt 1 ]; then
+        echo -e "ERROR: Multi-processor debugging is currently disabled." &> ${runlog}
+        exit 1
+    else
+        gdb --args ${bindir}/${prog} $extra
+    fi
+else
+    mpiexec -n ${np} ${bindir}/${prog} $extra &> ${runlog}
+fi
+
+# Signal success to the clean-up function.
+stage=${success}
+
+popd &> /dev/null
