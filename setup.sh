e9e36b2826229a4e94ec0b014fb9fb20e61cadfe
Matthew Young <matthew.young@unh.edu>
2023-12-08 18:01:26 -0500

    Update setup.sh
    * Remove --dry-run
    * Add --with-petsc-dir and --with-petsc-arch for SLEPc installation
    * Define script_dir
    * Refactor names of download targets
    * Edit success message
    * Define print_width and print_header
    * Define create_subdir
    * Extract build_petsc and build_slepc
    * Refactor intall_ext_dep

--- setup.sh
+++ setup.sh
@@ -25,15 +25,22 @@ endul=$(tput rmul)
 # Extract the name of this script.
 cli=${0##*/}
 
-# Define and create the setup log.
-setuplog="setup.log"
-> ${setuplog}
+# Determine this script's parent directory.
+script_dir="$(dirname "$(readlink -f "${0}")")"
 
 # Define the directory that will hold dependencies.
-ecspert_deps=~/ecspert/deps
+ecspert_deps=${script_dir}/deps
+
+# Define and create the setup log.
+setuplog=${script_dir}/"setup.log"
+> ${setuplog}
+
+# Define the value to use for PETSC_ARCH with --download-petsc.
 petsc_arch=arch-ecspert
 
 # Define the target PETSc and SLEPc packages to download, if necessary.
+petsc_version="3.20.2"
+slepc_version="3.20.1"
 petsc_package=petsc-3.20.2.tar.gz
 slepc_package=slepc-3.20.1.tar.gz
 
@@ -41,6 +48,8 @@ slepc_package=slepc-3.20.1.tar.gz
 verbose=0
 download_petsc=0
 download_slepc=0
+petsc_dir=
+petsc_arch=
 
 # This is the CLI's main help text.
 show_help()
@@ -65,8 +74,10 @@ ${textbf}DESCRIPTION${textnm}
         ${textbf}--download-slepc${textnm}
                 Download and configure SLEPc.
                 This will install SLEPc in ${ecspert_deps}, under arch-ecspert.
-        ${textbf}--dry-run${textnm}
-                Display the sequence of commands to be executed.
+        ${textbf}--with-petsc-dir DIR${textnm}
+                Use DIR as PETSC_DIR when building SLEPc. Ignored with --download-petsc.
+        ${textbf}--with-petsc-arch ARCH${textnm}
+                Use ARCH as PETSC_ARCH when building SLEPc. Ignored with --download-petsc.
 "
 }
 
@@ -81,8 +92,8 @@ TEMP=$(getopt \
     -n 'build.sh' \
     -o 'hv' \
     -l 'help,verbose' \
-    -l 'download-petsc' \
-    -l 'download-slepc' \
+    -l 'download-petsc,download-slepc' \
+    -l 'with-petsc-dir:,with-petsc-arch:' \
     -- "$@")
 
 if [ $? -ne 0 ]; then
@@ -112,6 +123,16 @@ while [ $# -gt 0 ]; do
         '--download-slepc')
             download_slepc=1
             shift
+            continue
+        ;;
+        '--with-petsc-dir')
+            export PETSC_DIR="${2}"
+            shift 2
+            continue
+        ;;
+        '--with-petsc-arch')
+            export PETSC_ARCH="${2}"
+            shift 2
             continue
         ;;
         '--')
@@ -156,8 +177,16 @@ cleanup() {
     else
         echo 
         echo -e "${u_success} Success! ${u_success}"
+        echo
+        echo "You may want to set the following environment variables in your start-up file."
+        echo "PETSC_DIR: \"${PETSC_DIR}\""
+        echo "SLEPC_DIR: \"${SLEPC_DIR}\""
         echo 
-        echo "You may now try building the simulation or potential solver by running"
+        echo "For example, if you're running bash, add the following lines to ~/.bashrc"
+        echo "export PETSC_DIR=\"${PETSC_DIR}\""
+        echo "export SLEPC_DIR=\"${SLEPC_DIR}\""
+        echo 
+        echo "You should now try building the simulation or potential solver by running"
         echo "$ ./build.sh -p {pic,solver} [OPTIONS]"
     fi
 }
@@ -171,7 +200,20 @@ mark_stage() {
     fi
 }
 
-stage="installation"
+print_width() {
+    local c=${1}
+
+    printf "=%.0s" $(seq 1 $COLUMNS)
+    echo 
+}
+
+print_header() {
+    print_width "="
+    if [ -n "${@}" ]; then
+        echo -e ${@}
+        print_width "="
+    fi
+}
 
 # Adapted from https://stackoverflow.com/a/3352015. This will trim leading and
 # trailing whitespace from a given string. For example: `trim "  a b  "` yields
@@ -185,30 +227,37 @@ trim() {
     printf '%s' "$var"
 }
 
-# The function that will download and build external dependencies.
-install_ext_deps() {
-    if [ -z "${1}" ]; then
-        echo "${u_error} Missing path to external-dependencies directory."
-        exit 1
+create_subdir() {
+    local response
+    local answer
+
+    subdir="${1}"
+    if [ -z "${subdir}" ]; then
+        return 0
     fi
-    local deps_dir="${1}"
-    local tmp_name=tmp
 
-    # Create and enter the top-level directory for external dependencies.
-    mkdir -p ${deps_dir}
-    pushd ${deps_dir} &> /dev/null
+    echo "Create ${subdir} subdirectory?"
+    printf "> "
+    read response
+    echo 
+    answer="$(trim "${response}")"
+    case ${answer} in
+        y|Y|yes|Yes)
+            mkdir -p dev
+        ;;
+        n|N|no|No)
+        ;;
+        *)
+            echo "Please answer \"y/Y/yes/Yes\" or \"n/N/no/No\""
+            create_subdir ${subdir}
+        ;;
+    esac
+}
 
-    # Create and enter the local subdirectory where this script will download
-    # and build each package. Isolating the source code allows us to remove it
-    # while leaving the build dependencies.
-    mkdir ${tmp_name}
-    pushd ${tmp_name} &> /dev/null
+stage="installation"
 
-    # --> PETSc
-    wget https://web.cels.anl.gov/projects/petsc/download/release-snapshots/petsc_package
-    tar -xvzf petsc_package
-    pushd ${ecspert_deps}/petsc &> /dev/null
-    export PETSC_ARCH=${petsc_arch} \
+build_petsc() {
+    export PETSC_DIR=$(pwd)
     ./configure \
     --download-cmake \
     --download-fblaslapack \
@@ -216,31 +265,80 @@ install_ext_deps() {
     --download-scalapack \
     --download-mumps \
     --download-hypre \
-    && make all check
-    popd &> /dev/null
-    export PETSC_DIR=${ecspert_deps}/petsc
+    &>> ${setuplog} \
+    && make all check \
+    &>> ${setuplog}
+}
+
+build_slepc() {
+    export SLEPC_DIR=$(pwd)
+    ./configure &>> ${setuplog} \
+    && make SLEPC_DIR=${SLEPC_DIR} PETSC_DIR=${PETSC_DIR} PETSC_ARCH=${PETSC_ARCH} \
+    &>> ${setuplog} \
+    && make SLEPC_DIR=${SLEPC_DIR} PETSC_DIR=${PETSC_DIR} check \
+    &>> ${setuplog}
+}
+
+# The function that will download and build external dependencies.
+install_ext_dep() {
+    if [ -z "${1}" ]; then
+        return 0
+    fi
+
+    local package_name="${1}"
+
+    # Create and enter the top-level directory for external dependencies.
+    mkdir -p ${ecspert_deps}
+    pushd ${ecspert_deps} &> /dev/null
+
+    # --> PETSc
+    if [ ${package_name} == "petsc" ]; then
+        print_header "Downloading PETSc"
+        archive="petsc-${petsc_version}.tar.gz"
+        wget https://web.cels.anl.gov/projects/petsc/download/release-snapshots/${archive}
+        print_header "Unpacking PETSc"
+        tar -xvzf ${archive} &>> ${setuplog}
+        ln -s "petsc-${petsc_version}" petsc
+        pushd ${ecspert_deps}/petsc &> /dev/null
+        print_header "Building PETSc\nThis may take tens of minutes"
+        build_petsc
+        popd &> /dev/null
+    fi
 
     # --> SLEPc
-    wget https://slepc.upv.es/download/distrib/slepc_package
-    tar -xvzf slepc_package
-    pushd ${ecspert_deps}/slepc &> /dev/null
-    ./configure
-    popd &> /dev/null
-    export SLEPC_DIR=${ecspert_deps}/slepc
-
-    # Exit and remove the temporary source-code directory.
-    popd &> /dev/null
-    /bin/rm -rf ${tmp_name}
+    if [ ${package_name} == "slepc" ]; then
+        print_header "Downloading SLEPc"
+        archive="slepc-${slepc_version}.tar.gz"
+        wget https://slepc.upv.es/download/distrib/${archive}
+        print_header "Unpacking SLEPc"
+        tar -xvzf ${archive} &>> ${setuplog}
+        ln -s "slepc-${slepc_version}" slepc
+        pushd ${ecspert_deps}/slepc &> /dev/null
+        print_header "Building SLEPc\nThis may take a few minutes"
+        build_slepc
+        popd &> /dev/null
+    fi
 
     # Exit the top-level external-dependencies directory.
     popd &> /dev/null
 }
 
-# Design Questions
-# - Should we reduce the options to a single --download-deps option?
-# - Should we split install_ext_deps into a function for each dependency or
-#   should we pass a flag to indicate which package(s) to install?
+# Download and install PETSc, if requested.
 if [ -n "${download_petsc}" ]; then
-    # TODO
+    install_ext_dep "petsc"
 fi
+
+# Download and install SLEPc, if requested.
+if [ -n "${download_slepc}" ]; then
+    install_ext_dep "slepc"
+fi
+
+# Offer to create git-ignored dev directory.
+echo "Do you want to create a dev/ subdirectory? Git will automatically ignore the contents"
+echo "of this subdirectory. It can be useful for storing files that are specific to your"
+echo "copy of this repository (e.g., notes or temporary scripts)."
+create_subdir "dev"
+
+
+stage=$success
 
