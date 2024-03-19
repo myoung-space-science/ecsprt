5dad48c28ccba4e515e5e6fd572a0010e5b103f5
Matthew Young <myoung.space.science@gmail.com>
2024-03-19 14:09:30 -0400

    Create moments.{c,h}

---
+++ src/moments.h
@@ -0,0 +1,9 @@
+#ifndef COLLECT_H
+#define COLLECT_H
+
+#include "ecsprt.h"
+
+extern PetscErrorCode Collect2DFluidMoments(void *opts);
+extern PetscErrorCode Collect3DFluidMoments(void *opts);
+
+#endif // COLLECT_H
