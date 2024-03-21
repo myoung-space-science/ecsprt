c27bf4fc1b44290b0e5b7e756e90885dcb34d677
Matthew Young <myoung.space.science@gmail.com>
2024-03-21 14:04:28 -0400

    Update test_all.sh
    * Change "processors" to "processor(s)"
    * Test sobol and sinusoidal initial distributions
    * Add initial distribution to test name

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -3,49 +3,55 @@
 cwd=$(dirname $(realpath $0))
 rundir=${cwd}/runs
 
-name=pic-gmres-hypre-3D
+base=pic-gmres-hypre-3D
 for n in 1 2; do
-    ./run.sh -n $n -p pic -r $rundir -o $name \
-        --options ${cwd}/grid3D.ini \
-        --options ${cwd}/gmres-hypre.ini \
-        --options ${cwd}/fb-physics.ini \
-        --require-options \
-        args \
-        -Nt 2 \
-        -dt 1e-5
-    success=$?
-    # TODO: Further analyze output before declaring success.
-    if [ $success == 0 ]; then
-        echo "Test $name passed on $n processors"
-        rm -rf $rundir/$name
-        rm -f $rundir/latest
-    else
-        echo "Test $name failed on $n processors"
-        exit 1
-    fi
+    for dist in sobol sinusoidal; do
+        name="$base-$dist"
+        ./run.sh -n $n -p pic -r $rundir -o $name \
+            --options ${cwd}/grid3D.ini \
+            --options ${cwd}/gmres-hypre.ini \
+            --options ${cwd}/fb-physics.ini \
+            --require-options \
+            args \
+            -Nt 2 \
+            -dt 1e-5
+        success=$?
+        # TODO: Further analyze output before declaring success.
+        if [ $success == 0 ]; then
+            echo "Test $name passed on $n processor(s)"
+            rm -rf $rundir/$name
+            rm -f $rundir/latest
+        else
+            echo "Test $name failed on $n processor(s)"
+            exit 1
+        fi
+    done
 done
 
-name=solver-gmres-hypre-3D
+base=solver-gmres-hypre-3D
 for n in 1 2; do
-    ./run.sh -n $n -p solver -r $rundir -o $name \
-        --options ${cwd}/grid3D.ini \
-        --options ${cwd}/gmres-hypre.ini \
-        --options ${cwd}/fb-physics.ini \
-        --require-options \
-        args \
-        --input ~/ecsprt/test-data/cos_xyz_32x32x32.h5 \
-        --lhs-eigenvalues \
-        --flux-scale 100.0
-    success=$?
-    # TODO: Further analyze output before declaring success.
-    if [ $success == 0 ]; then
-        echo "Test $name passed on $n processors"
-        rm -rf $rundir/$name
-        rm -f $rundir/latest
-    else
-        echo "Test $name failed on $n processors"
-        exit 1
-    fi
+    for dist in sobol sinusoidal; do
+        name="$base-$dist"
+        ./run.sh -n $n -p solver -r $rundir -o $name \
+            --options ${cwd}/grid3D.ini \
+            --options ${cwd}/gmres-hypre.ini \
+            --options ${cwd}/fb-physics.ini \
+            --require-options \
+            args \
+            --input ~/ecsprt/test-data/cos_xyz_32x32x32.h5 \
+            --lhs-eigenvalues \
+            --flux-scale 100.0
+        success=$?
+        # TODO: Further analyze output before declaring success.
+        if [ $success == 0 ]; then
+            echo "Test $name passed on $n processor(s)"
+            rm -rf $rundir/$name
+            rm -f $rundir/latest
+        else
+            echo "Test $name failed on $n processor(s)"
+            exit 1
+        fi
+    done
 done
 
 # TODO:
