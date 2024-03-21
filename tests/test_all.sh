7ec11bf868502d3482eb7dc6fce71be257c2c2a2
Matthew Young <myoung.space.science@gmail.com>
2024-03-21 15:09:31 -0400

    Update test_all.sh
    * Add 2D cases
    * Refactor

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -3,58 +3,57 @@
 cwd=$(dirname $(realpath $0))
 rundir=${cwd}/runs
 
-base=pic-gmres-hypre-3D
-for n in 1 2; do
-    for dist in sobol sinusoidal; do
-        name="$base-$dist"
-        ./run.sh -n $n -p pic -r $rundir -o $name \
-            --options ${cwd}/grid3D.ini \
-            --options ${cwd}/gmres-hypre.ini \
-            --options ${cwd}/fb-physics.ini \
-            --require-options \
-            args \
-            -Nt 2 \
-            -dt 1e-5
-        success=$?
-        # TODO: Further analyze output before declaring success.
-        if [ $success == 0 ]; then
-            echo "Test $name passed on $n processor(s)"
-            rm -rf $rundir/$name
-            rm -f $rundir/latest
-        else
-            echo "Test $name failed on $n processor(s)"
-            exit 1
-        fi
-    done
-done
-
-base=solver-gmres-hypre-3D
-for n in 1 2; do
-    for dist in sobol sinusoidal; do
-        name="$base-$dist"
-        ./run.sh -n $n -p solver -r $rundir -o $name \
-            --options ${cwd}/grid3D.ini \
-            --options ${cwd}/gmres-hypre.ini \
-            --options ${cwd}/fb-physics.ini \
-            --require-options \
-            args \
-            --input ~/ecsprt/test-data/cos_xyz_32x32x32.h5 \
-            --lhs-eigenvalues \
-            --flux-scale 100.0
-        success=$?
-        # TODO: Further analyze output before declaring success.
-        if [ $success == 0 ]; then
-            echo "Test $name passed on $n processor(s)"
-            rm -rf $rundir/$name
-            rm -f $rundir/latest
-        else
-            echo "Test $name failed on $n processor(s)"
-            exit 1
-        fi
-    done
-done
-
 # TODO:
-# - test solver very small case with --view-lhs
 # - test solver small case with --lhs-eigenvalues
-# - test all cases in 2D when possible
+# - test solver very small case with --view-lhs
+
+for N in 2 3; do
+    for n in 1 2; do
+        for dist in sobol sinusoidal; do
+
+            name="pic-gmres-hypre-${N}D-$dist"
+            ./run.sh -n $n -p pic -r $rundir -o $name \
+                --options ${cwd}/grid3D.ini \
+                --options ${cwd}/gmres-hypre.ini \
+                --options ${cwd}/fb-physics.ini \
+                --require-options \
+                args \
+                -ndim $N \
+                -Nt 2 \
+                -dt 1e-5
+            success=$?
+            # TODO: Further analyze output before declaring success.
+            if [ $success == 0 ]; then
+                echo "Test $name passed on $n processor(s)"
+                rm -rf $rundir/$name
+                rm -f $rundir/latest
+            else
+                echo "Test $name failed on $n processor(s)"
+                exit 1
+            fi
+
+            name="solver-gmres-hypre-${N}D-$dist"
+            ./run.sh -n $n -p solver -r $rundir -o $name \
+                --options ${cwd}/grid3D.ini \
+                --options ${cwd}/gmres-hypre.ini \
+                --options ${cwd}/fb-physics.ini \
+                --require-options \
+                args \
+                --input ~/ecsprt/test-data/cos_xyz_32x32x32.h5 \
+                --lhs-eigenvalues \
+                --flux-scale 100.0
+            success=$?
+            # TODO: Further analyze output before declaring success.
+            if [ $success == 0 ]; then
+                echo "Test $name passed on $n processor(s)"
+                rm -rf $rundir/$name
+                rm -f $rundir/latest
+            else
+                echo "Test $name failed on $n processor(s)"
+                exit 1
+            fi
+
+        done
+    done
+done
+
