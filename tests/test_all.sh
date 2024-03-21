8e2e2088b8d5bee51c0f51f9483427f5a40b52c3
Matthew Young <myoung.space.science@gmail.com>
2024-03-21 13:24:00 -0400

    Update test_all.sh
    * Store current working directory
    * Fix options-reading bug
    * Require options files to exist

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-rundir=$(dirname $(realpath $0))/runs
+cwd=$(dirname $(realpath $0))
+rundir=${cwd}/runs
 
 name=pic-gmres-hypre-3D
 for n in 1 2; do
-    ./run.sh -n 2 -p pic -r $rundir -o $name \
-        --options grid3D.ini \
-        --options gmres-hypre.ini \
-        --options fb-physics.ini \
+    ./run.sh -n $n -p pic -r $rundir -o $name \
+        --options ${cwd}/grid3D.ini \
+        --options ${cwd}/gmres-hypre.ini \
+        --options ${cwd}/fb-physics.ini \
+        --require-options \
         args \
         -Nt 2 \
         -dt 1e-5
@@ -25,10 +27,11 @@ done
 
 name=solver-gmres-hypre-3D
 for n in 1 2; do
-    ./run.sh -n 2 -p solver -r $rundir -o $name \
-        --options grid3D.ini \
-        --options gmres-hypre.ini \
-        --options fb-physics.ini \
+    ./run.sh -n $n -p solver -r $rundir -o $name \
+        --options ${cwd}/grid3D.ini \
+        --options ${cwd}/gmres-hypre.ini \
+        --options ${cwd}/fb-physics.ini \
+        --require-options \
         args \
         --input ~/ecsprt/test-data/cos_xyz_32x32x32.h5 \
         --lhs-eigenvalues \
