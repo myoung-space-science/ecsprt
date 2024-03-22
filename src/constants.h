600e503e2a3c114e38b0da4d0dd50f0563fcf27d
Matthew Young <myoung.space.science@gmail.com>
2024-03-22 17:07:44 -0400

    Remove NDIM definition from constants.h

--- src/constants.h
+++ src/constants.h
@@ -1,7 +1,6 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#define NDIM 3
 #define Q 1.6022e-19   // fundamental charge in C
 #define MP 1.6726e-27  // proton mass in kg
 #define ME 9.1094e-31  // electron mass in kg
