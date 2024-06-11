735f45834be70915c305cb4660645458a53de110
Matthew Young <myoung.space.science@gmail.com>
2024-06-11 14:11:09 -0400

    Create OutputFunc typedef

--- src/file-io.h
+++ src/file-io.h
@@ -11,4 +11,6 @@ extern PetscErrorCode OutputSwarmBinary(const char *insert, Context *ctx);
 extern PetscErrorCode OutputNoOp(const char *insert, Context *ctx);
 extern PetscErrorCode ViewLHS(KSP ksp, Context *ctx);
 
+typedef PetscErrorCode (*OutputFunc)(const char *s, Context *ctx);
+
 #endif // FILE_IO_H
